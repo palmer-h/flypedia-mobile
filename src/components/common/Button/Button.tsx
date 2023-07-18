2458a43d9d041fec597163c8fc4c6856d9c15ec6
Palmer Hinchliffe <palmer.hinchliffe@rotamaster.co.uk>
2023-07-18 23:11:44 +0100

    fix: moving fn inline

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -5,19 +5,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import styles from '~/components/common/Button/styles';
 import theme from '~/theme';
 
-const Button: React.FC<Props> = props => {
-  return (
-    <View style={[styles.container, props.style]}>
-      {props.icon ? (
-        <FontAwesomeIcon
-          icon={props.icon}
-          style={styles.icon}
-          color={props.color ? String(props.color) : theme.colors.primary}
-        />
-      ) : null}
-      <RNButton {...props} color={props.color || theme.colors.primary} />
-    </View>
-  );
-};
+const Button: React.FC<Props> = props => (
+  <View style={[styles.container, props.style]}>
+    {props.icon ? (
+      <FontAwesomeIcon
+        icon={props.icon}
+        style={styles.icon}
+        color={props.color ? String(props.color) : theme.colors.primary}
+      />
+    ) : null}
+    <RNButton {...props} color={props.color || theme.colors.primary} />
+  </View>
+);
 
 export default Button;

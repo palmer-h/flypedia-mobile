d51dc8be709910bd870baf2942fa8eabf4bbec6c
Palmer Hinchliffe <palmer.hinchliffe@rotamaster.co.uk>
2023-07-18 23:10:40 +0100

    fix: moving fn inline

--- src/components/common/OrbCardListItem/OrbCardListItem.tsx
+++ src/components/common/OrbCardListItem/OrbCardListItem.tsx
@@ -6,35 +6,29 @@ import styles from '~/components/common/OrbCardListItem/styles';
 import ImageOrb from '../ImageOrb/ImageOrb';
 import colors from '~/theme/colors';
 
-const OrbCardListItem: React.FC<Props> = props => {
-  const handlePress = (): void => {
-    props.onPress?.(props.id);
-  };
-
-  return (
-    <Pressable
-      style={({ pressed }) => [
-        styles.container,
-        props.style,
-        {
-          backgroundColor: pressed ? colors.pressed : colors.background,
-        },
-      ]}
-      accessibilityLabel={props.accessibilityLabel}
-      accessibilityHint={props.accessibilityHint}
-      onPress={handlePress}>
-      <View style={styles.contentContainer}>
-        <Text style={theme.typography.titleMedium}>{props.title}</Text>
-        {props.subtitle ? (
-          <Text style={theme.typography.subtitleMedium}>{props.subtitle}</Text>
-        ) : null}
-        <Text style={[theme.typography.bodyMedium, styles.descriptionText]}>
-          {props.desc}
-        </Text>
-      </View>
-      <ImageOrb size={50} imgSrc={props.orbImgSrc} shadow />
-    </Pressable>
-  );
-};
+const OrbCardListItem: React.FC<Props> = props => (
+  <Pressable
+    style={({ pressed }) => [
+      styles.container,
+      props.style,
+      {
+        backgroundColor: pressed ? colors.pressed : colors.background,
+      },
+    ]}
+    accessibilityLabel={props.accessibilityLabel}
+    accessibilityHint={props.accessibilityHint}
+    onPress={() => props.onPress?.(props.id)}>
+    <View style={styles.contentContainer}>
+      <Text style={theme.typography.titleMedium}>{props.title}</Text>
+      {props.subtitle ? (
+        <Text style={theme.typography.subtitleMedium}>{props.subtitle}</Text>
+      ) : null}
+      <Text style={[theme.typography.bodyMedium, styles.descriptionText]}>
+        {props.desc}
+      </Text>
+    </View>
+    <ImageOrb size={50} imgSrc={props.orbImgSrc} shadow />
+  </Pressable>
+);
 
 export default OrbCardListItem;
